a602333e2e9d966e02b7299d8c8b99790ae24776
Casey Portebois <Casey.Portebois@asmeg.org>
2025-01-09 09:33:01 +0100

    Ajout de toutes les fonctionnalités demandées : contact, graphiques, histogramme et commits

--- __init__.py
+++ __init__.py
@@ -11,6 +11,57 @@ app = Flask(__name__)
 def hello_world():
     return render_template('hello.html') 
   
+@app.route("/contact/")
+def MaPremiereAPI():
+    return render_template('contact.html')
+
+@app.route('/tawarano/')
+def meteo():
+    response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
+    raw_content = response.read()
+    json_content = json.loads(raw_content.decode('utf-8'))
+    results = []
+    for list_element in json_content.get('list', []):
+        dt_value = list_element.get('dt')
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
+        results.append({'Jour': dt_value, 'temp': temp_day_value})
+    return jsonify(results=results)
+
+@app.route("/rapport/")
+def mongraphique():
+    return render_template("graphique.html")
+
+@app.route("/histogramme/")
+def monhistogramme():
+    return render_template("histogramme.html")
+
+@app.route('/commits-data/')
+def get_commits_data():
+    response = urlopen('https://api.github.com/repos/OpenRSI/5MCSI_Metriques/commits')
+    raw_content = response.read()
+    json_content = json.loads(raw_content.decode('utf-8'))
+    
+    # Créer un dictionnaire pour compter les commits par minute
+    commits_by_minute = {}
+    
+    for commit in json_content:
+        date_string = commit['commit']['author']['date']
+        date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
+        minute_key = date_object.strftime('%Y-%m-%d %H:%M')
+        
+        if minute_key in commits_by_minute:
+            commits_by_minute[minute_key] += 1
+        else:
+            commits_by_minute[minute_key] = 1
+    
+    # Convertir en liste pour le graphique
+    results = [{'minute': k, 'count': v} for k, v in commits_by_minute.items()]
+    return jsonify(results=results)
+
+@app.route("/commits/")
+def commits_graph():
+    return render_template("commits.html")
+  
 if __name__ == "__main__":
   app.run(debug=True)
 #commit
